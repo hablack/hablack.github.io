e0a1fa730614ca4f8344fd9b28aebd430aa2162d
hablack <hadleyblack92@gmail.com>
2024-03-13 11:13:58 -0700

    Add files via upload

---
+++ headers.js
@@ -0,0 +1,19 @@
+document.write('\
+\
+<TABLE border="0" width="700" align="center">\
+   <TR>\
+      <TD width="100%" align="left" valign="top">\
+		<h1 align="center">\
+		   		<b>\
+		   		<a href="index.html">Home</a> .\
+		   		<a href="publication.html">Publications</a> .\
+ 		   		<a href="bio.html">Bio</a> .\
+          <a href="teaching.html">Teaching</a> .\
+         <a href="oaq.html">OAQ</a> .\
+          <a href="http://ptreview.sublinear.info/">PTReview</a> \
+</b></h1>\
+<br>\
+     </td>\
+   </tr>\
+</table>\
+');
